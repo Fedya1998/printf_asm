7a497b3eb71126180c1a267780f585813f975a4e
Liza <zlobina.ea@phystech.edu>
2017-09-07 22:52:33 +0300

    n

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 extern void printff();
 
 int main()
-{
+{	
+    //--------	
     printff("TEst hex, dec, char, string, bin %x %d%c %s %b. %c %s %x %d %%\n", 5516, 100, '!', "STRING\n", 18, 'I', "love", 3802, 100);
     //printff("Test %d", 100);
 	return 0;
